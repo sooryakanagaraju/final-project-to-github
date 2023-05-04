19694a3687e14991da21a178fa37ae1f9ddfc536
sooryakanagaraju <sooryakanagaraj11@gmail.com>
2023-05-04 17:15:48 +0530

    review to each product done!!!

---
+++ milestone-4/src/app/shared/services/movie.service.ts
@@ -0,0 +1,36 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+  httpOptions:any;
+  public search = new BehaviorSubject<string>("");
+  constructor(private http:HttpClient) { 
+
+      this.httpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+          'Authorization': "Bearer "+sessionStorage.getItem("token")
+        })
+      }
+    }
+  public getProducts():Observable<any>{
+  
+    return this.http.get("http://localhost:4500/moviess",this.httpOptions);
+  }
+
+  public getProducts2(pattern:string):Observable<any>{
+    return this.http.get("http://localhost:4500/660/moviess?type="+pattern,this.httpOptions);
+  }
+
+  getDetails(id:number):Observable<any>{
+    return this.http.get("http://localhost:4500//details//"+id,this.httpOptions)
+  }
+
+  getMoviesById(id:number):Observable<any>{
+    return this.http.get("http://localhost:4500//moviess//"+id,this.httpOptions)
+  }
+}
